397e86967d7a237399d4a2a47c848bc065f7d512
Volker Schoenefeld <volker@limbicsoft.com>
2011-06-15 16:36:42 -0700

    updated initial settings

--- limbicgl/config.h
+++ limbicgl/config.h
@@ -10,7 +10,6 @@
 // This can be used to run gamecenter in a separate GCD queue. This is for testing, and doesn't really seem to have any effect on the stuttering.
 //#define GAMECENTER_WITH_GCD
 
-
 #define SINGLETHREADEDDRIVER 0
 #define GCDDRIVER 1
 #define THREADEDDRIVER 2
@@ -32,7 +31,7 @@
 #define RENDERER_LOAD 100
 
 // This can be used to get very verbose traces for debugging
-#define VERBOSE_LOG
+//#define VERBOSE_LOG
 
 #ifdef VERBOSE_LOG
 #   define VerboseLog(fmt, ...) NSLog((@"%s " fmt), __PRETTY_FUNCTION__, ##__VA_ARGS__);
